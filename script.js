fd045f79c95f359a48f02e2d5c887526affd1fb5
clostoUSRF <smapped345@gmail.com>
2025-08-18 02:36:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('matrix');
 const ctx = canvas.getContext('2d');
 let w,h,columns,drops,fontSize=16;
-const glyphs = '40K319DSNW9023NDC0C98AE73J33D;
+const glyphs = '40K?19D%NW&023N$C0C98@E73J33D;
 function resize(){
   w = canvas.width = window.innerWidth;
   h = canvas.height = window.innerHeight;
@@ -24,4 +24,5 @@ function draw(){
   requestAnimationFrame(draw);
 }
 draw();
+
 
